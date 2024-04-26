9bb9c5107cab13f3d07e69b556d07f9fc5f69f42
Diogo Lisboa <diogo@dlisboa.com>
2024-04-26 15:17:45 -0300

    Google Cloud Function template

---
+++ gcloud/function/func.go
@@ -0,0 +1,21 @@
+package hello
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/GoogleCloudPlatform/functions-framework-go/functions"
+)
+
+func init() {
+	functions.HTTP("HelloHTTP", helloHTTP)
+}
+
+// helloHTTP is an HTTP Cloud Function.
+func helloHTTP(w http.ResponseWriter, r *http.Request) {
+	name := r.URL.Query().Get("name")
+	if name == "" {
+		name = "World"
+	}
+	fmt.Fprintf(w, "Hello, %s!", name)
+}
