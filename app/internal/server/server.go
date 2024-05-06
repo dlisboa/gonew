f2cc81086aba69d1ff8b2b0d899baf40d43b9cac
Diogo Lisboa <diogo@dlisboa.com>
2024-05-06 10:18:58 -0300

    Improvements to the app template

--- app/internal/server/server.go
+++ app/internal/server/server.go
@@ -5,14 +5,14 @@ import (
 	"os"
 )
 
-func Run() {
+func Run() error {
 	assets := http.FileServer(http.Dir("./web"))
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("hello"))
 	})
-	mux.HandleFunc("GET /assets/", assets.ServeHTTP)
+	mux.Handle("GET /static/", http.StripPrefix("/static/", assets))
 
-	http.ListenAndServe(os.Getenv("APP_LISTEN_ADDR"), mux)
+	return http.ListenAndServe(os.Getenv("APP_LISTEN_ADDR"), mux)
 }
